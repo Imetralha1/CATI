a85a9a234b66fd7e32ce366af62851d8a3a4f203
SamBBret <samuelbret@icloud.com>
2026-06-04 12:06:48 +0100

    same

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@
 		.then(menu => {
 
 			const currentPage = window.location.pathname.split("/").pop();
-            console.log("Current page:", currentPage);
+			console.log("Current page:", window.location.pathname);
 			const path = findPath(menu, currentPage);
 			console.log(menu)
 			const marked = path ? markActive(menu, path) : menu;
